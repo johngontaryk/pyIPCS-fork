e8d19511d5dde16e3dfb4079ef4c5b0cf5962922
John Gontaryk <johngontaryk@gmail.com>
2026-07-23 20:01:44 -0400

    init
    Signed-off-by: John Gontaryk <johngontaryk@gmail.com>

--- src/pyipcs/__init__.py
+++ src/pyipcs/__init__.py
@@ -9,4 +9,7 @@ if sys.platform != "zos":
     raise RuntimeError("This package is only supported on z/OS.")
 
 from ._version import __version__
+from . import util
+from . import exceptions
 from .session import IpcsSession
+from .allocation import IpcsAllocation
