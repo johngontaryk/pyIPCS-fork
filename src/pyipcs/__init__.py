a50ae228befbc24602fd37b42a56e0678ce9e718
John Gontaryk <johngontaryk@gmail.com>
2026-07-27 21:37:49 -0400

    dump import
    Signed-off-by: John Gontaryk <johngontaryk@gmail.com>

--- src/pyipcs/__init__.py
+++ src/pyipcs/__init__.py
@@ -11,5 +11,6 @@ if sys.platform != "zos":
 from ._version import __version__
 from . import util
 from . import exceptions
+from .allocation import IpcsAllocation,
+from .dump import IpcsDump
 from .session import IpcsSession
-from .allocation import IpcsAllocation
